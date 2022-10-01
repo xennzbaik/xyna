c0313369c2a68fece469e51de579afb6fa24f783
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-10-02 03:29:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ global.loadDatabase = async function loadDatabase() {
 }
 loadDatabase()
 
-global.authFile = `${opts._[0] || 'pentagram'}.data.json`
+global.authFile = `${opts._[0] || 'session'}.data.json`
 console.log(`Load AuthFile from ${authFile}`)
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
